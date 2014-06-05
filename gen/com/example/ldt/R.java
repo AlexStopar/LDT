9226622e05a82d2a8733eee111f93f0211144e51
Alex Stopar <stopar.2@osu.edu>
2014-06-05 14:13:00 -0400

    I made me some Hours/Beep Screen. Will code over the weekend.
    Love, Alex

--- gen/com/example/ldt/R.java
+++ gen/com/example/ldt/R.java
@@ -1092,12 +1092,16 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05004c;
+        public static final int action_settings=0x7f050054;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
+        public static final int beep_duration=0x7f05003f;
+        public static final int beep_durationPicker=0x7f050040;
+        public static final int beep_volume=0x7f050041;
+        public static final int beep_volumePicker=0x7f050042;
         public static final int beginning=0x7f050011;
-        public static final int blink1=0x7f05004b;
-        public static final int cancel_button=0x7f050047;
+        public static final int blink1=0x7f050053;
+        public static final int cancel_button=0x7f05004f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1111,25 +1115,28 @@ containing a value of this type.
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
+        public static final int hourPicker=0x7f05003e;
+        public static final int hours=0x7f05003d;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int image1=0x7f05004a;
+        public static final int image1=0x7f050052;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int login_button=0x7f050043;
+        public static final int login_button=0x7f05004b;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
-        public static final int new_user_button=0x7f050042;
+        public static final int new_user_button=0x7f05004a;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int password=0x7f050048;
-        public static final int password_confirm=0x7f050049;
-        public static final int password_text=0x7f050041;
+        public static final int password=0x7f050050;
+        public static final int password_confirm=0x7f050051;
+        public static final int password_text=0x7f050049;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int register_button=0x7f050046;
+        public static final int register_button=0x7f05004e;
+        public static final int return_button=0x7f050044;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1147,16 +1154,17 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
-        public static final int textView2=0x7f05003e;
-        public static final int textView3=0x7f05003f;
-        public static final int textView4=0x7f050044;
+        public static final int test_beep_button=0x7f050043;
+        public static final int textView1=0x7f050045;
+        public static final int textView2=0x7f050046;
+        public static final int textView3=0x7f050047;
+        public static final int textView4=0x7f05004c;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int username=0x7f050045;
-        public static final int username_text=0x7f050040;
+        public static final int username=0x7f05004d;
+        public static final int username_text=0x7f050048;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1209,19 +1217,22 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_login=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int activity_new_user=0x7f03001a;
-        public static final int activity_splash_screen=0x7f03001b;
-        public static final int fragment_login=0x7f03001c;
-        public static final int fragment_main=0x7f03001d;
-        public static final int fragment_new_user=0x7f03001e;
-        public static final int fragment_splash_screen=0x7f03001f;
-        public static final int support_simple_spinner_dropdown_item=0x7f030020;
+        public static final int activity_hours_beep_screen=0x7f030018;
+        public static final int activity_login=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int activity_new_user=0x7f03001b;
+        public static final int activity_splash_screen=0x7f03001c;
+        public static final int fragment_hours_beep_screen=0x7f03001d;
+        public static final int fragment_login=0x7f03001e;
+        public static final int fragment_main=0x7f03001f;
+        public static final int fragment_new_user=0x7f030020;
+        public static final int fragment_splash_screen=0x7f030021;
+        public static final int support_simple_spinner_dropdown_item=0x7f030022;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
-        public static final int splash_screen=0x7f0c0001;
+        public static final int hours_beep_screen=0x7f0c0000;
+        public static final int main=0x7f0c0001;
+        public static final int splash_screen=0x7f0c0002;
     }
     public static final class string {
         public static final int LDT=0x7f0a000e;
@@ -1268,11 +1279,14 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
+        public static final int beep_duration=0x7f0a0026;
+        public static final int beep_volume=0x7f0a0027;
         public static final int cancel=0x7f0a0023;
         public static final int confirm_password=0x7f0a0021;
         public static final int enter_password=0x7f0a0016;
         public static final int enter_username=0x7f0a0015;
         public static final int hello_world=0x7f0a0012;
+        public static final int hours=0x7f0a0025;
         public static final int login=0x7f0a001c;
         public static final int login_title=0x7f0a001d;
         public static final int new_account=0x7f0a001e;
@@ -1280,7 +1294,10 @@ containing a value of this type.
         public static final int password=0x7f0a0020;
         public static final int permission_launch_activity=0x7f0a001a;
         public static final int register=0x7f0a0022;
+        public static final int return_to_options=0x7f0a0029;
+        public static final int test_beep=0x7f0a0028;
         public static final int title_activity_database_login=0x7f0a0018;
+        public static final int title_activity_hours_beep_screen=0x7f0a0024;
         public static final int title_activity_login=0x7f0a001b;
         public static final int title_activity_main=0x7f0a0013;
         public static final int title_activity_new_user=0x7f0a0014;
@@ -2665,7 +2682,7 @@ containing a value of this type.
          */
         public static final int[] MenuView = {
             0x010100ae, 0x0101012c, 0x0101012d, 0x0101012e,
-            0x0101012f, 0x01010130, 0x01010131, 0x0101041a
+            0x0101012f, 0x01010130, 0x01010131, 0x01010435
         };
         /**
           <p>

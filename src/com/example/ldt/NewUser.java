5f59b1c35fafa2f5be0e6c2b1ddec4cd1860249c
shiva0603 <ganesh.28@buckeyemail.osu.edu>
2014-06-07 12:49:47 -0400

    Update NewUser.java
    Checked for Duplicate User
    Only one user can register per device

--- src/com/example/ldt/NewUser.java
+++ src/com/example/ldt/NewUser.java
@@ -5,7 +5,9 @@ import com.example.ldt.R;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings.Secure;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +20,8 @@ public class NewUser extends Activity implements OnClickListener{
 	private EditText etPassword;
 	private EditText etConfirm;
 	private LoginDatabase dh;
+	String deviceId;
+	long deviceIdCount=0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,10 @@ public class NewUser extends Activity implements OnClickListener{
 		btnAdd.setOnClickListener(this);
 		View btnCancel = (Button) findViewById(R.id.cancel_button);
 		btnCancel.setOnClickListener(this);
+		
+	     deviceId = Secure.getString(this.getContentResolver(),
+                Secure.ANDROID_ID);
+       // Toast.makeText(this, deviceId, Toast.LENGTH_SHORT).show();
 
 	}
 	
@@ -38,7 +46,6 @@ public class NewUser extends Activity implements OnClickListener{
 		switch (v.getId()) {
 		case R.id.register_button:
 			CreateAccount();
-			finish();
 			break;
 		case R.id.cancel_button:
 			finish();
@@ -51,18 +58,39 @@ public class NewUser extends Activity implements OnClickListener{
 			String username = etUsername.getText().toString();
 			String password = etPassword.getText().toString();
 			String confirm = etConfirm.getText().toString();
-			if ((password.equals(confirm)) && (!username.equals(""))
-					&& (!password.equals("")) && (!confirm.equals(""))) {
-				this.dh = new LoginDatabase(this);
-				this.dh.insert(username, password);
-				// this.labResult.setText("Added");
-				Toast.makeText(NewUser.this, "new record inserted",
-						Toast.LENGTH_SHORT).show();
-				finish();
-			} else if ((username.equals("")) || (password.equals(""))
+			
+
+			/*if(!(this.dh.getSingleEntry(username)))
+			{
+				new AlertDialog.Builder(this)
+				.setTitle("Error")
+				.setMessage("username already exists")
+				.setNeutralButton("Try Again",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+							}
+						})
+
+				.show();
+				return;
+			}	*/
+			
+			//this.dh.getSingleEntry(username,password);
+			this.dh = new LoginDatabase(this);
+				
+				long id =0;
+				if ((username.equals("")))
+					{
+				Toast.makeText(NewUser.this, "Username is empty", Toast.LENGTH_SHORT)
+				.show();
+				return;
+					}else if(password.equals("")
 					|| (confirm.equals(""))) {
-				Toast.makeText(NewUser.this, "Missing entry", Toast.LENGTH_SHORT)
+				Toast.makeText(NewUser.this, "Password field is empty", Toast.LENGTH_SHORT)
 						.show();
+				return;
+			
 			} else if (!password.equals(confirm)) {
 				new AlertDialog.Builder(this)
 						.setTitle("Error")
@@ -75,6 +103,59 @@ public class NewUser extends Activity implements OnClickListener{
 								})
 
 						.show();
+				return;
+			} else 
+		    id = dh.getSingleEntry(username, password);
+			if (id == 1)
+			{
+			    /*Toast.makeText(NewUser.this, "username already taken",
+			                        Toast.LENGTH_LONG).show();*/
+				
+				new AlertDialog.Builder(this)
+				.setTitle("Error")
+				.setMessage("Username already exists")
+				.setNeutralButton("Please try Again with different user name",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+							}
+						})
+
+				.show();
+				etUsername.getText().clear();
+				etPassword.getText().clear();
+				etConfirm.getText().clear();
+				etUsername.requestFocus();
+			    return;
+			}
+			
+			deviceIdCount = dh.getdeviceIdDuplicate(deviceId);
+			if (deviceIdCount == 1)
+			{
+				new AlertDialog.Builder(this)
+				.setTitle("Error")
+				.setMessage("User already registered")
+				.setNeutralButton("only one user allowed per device",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+							}
+						})
+
+				.show();
+				etUsername.requestFocus();
+			    return;				
+			}
+			
+			if ((password.equals(confirm)) && (!username.equals(""))
+					&& (!password.equals("")) && (!confirm.equals(""))) {
+				//this.dh = new LoginDatabase(this);
+				this.dh.insert(username, password, deviceId);
+				// this.labResult.setText("Added");
+				Toast.makeText(NewUser.this, "new record inserted",
+						Toast.LENGTH_SHORT).show();
+				finish();
 			}
 		}
+
 }
